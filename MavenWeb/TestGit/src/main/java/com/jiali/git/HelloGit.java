f25ddfb0ca160d6ad1f1c46bc805cc13ec22a920
xj-201 <xj-201@hotmail.com>
2021-02-07 12:22:16 +0800

    添加文件

---
+++ MavenWeb/TestGit/src/main/java/com/jiali/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.jiali.git;
+
+/**
+ * @author ishiali
+ * @create 2021-02-07 12:15
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("添加文件");
+    }
+}
